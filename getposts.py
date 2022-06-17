5cb77384e5eea2c706887f7b6328bdf57cd89443
privacy-dad <privacy-dad@protonmail.com>
2022-06-17 17:59:09 +0200

    first commit

---
+++ getposts.py
@@ -0,0 +1,17 @@
+import requests
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime as dt
+import json
+
+env = Environment(
+    loader=FileSystemLoader( searchpath="./templates" ),
+    autoescape=select_autoescape()
+)
+
+url = "https://ransomwhat.telemetry.ltd/posts"
+r = requests.get(url)
+template = env.get_template("temp1.html")
+ransoms = r.json()
+ransoms.reverse()
+with open('./index.html','w') as f:
+            f.write(template.render(ransoms=ransoms,fecha=dt.now().strftime('%d-%b-%Y %H:%M')))
