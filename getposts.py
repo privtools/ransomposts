d6474802f0f27142264d74be076d95f680060bec
privacy-dad <privacy-dad@protonmail.com>
2022-06-18 13:06:17 +0200

    update getposts to show timezone in index

--- getposts.py
+++ getposts.py
@@ -1,6 +1,7 @@
 import requests
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime as dt
+from datetime import timezone
 import json
 
 env = Environment(
@@ -14,4 +15,4 @@ template = env.get_template("temp1.html")
 ransoms = r.json()
 ransoms.reverse()
 with open('./index.html','w') as f:
-            f.write(template.render(ransoms=ransoms,fecha=dt.now().strftime('%d-%b-%Y %H:%M')))
+            f.write(template.render(ransoms=ransoms,fecha=dt.now(tz=timezone.utc).strftime('%d-%b-%Y %H:%M %Z')))
