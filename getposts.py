c9de3590eefb7c14ff953313fe4185946403882b
privacy-dad <privacy-dad@protonmail.com>
2022-07-26 22:41:26 +0200

    add link to ransom groups

--- getposts.py
+++ getposts.py
@@ -9,10 +9,29 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
+url = "https://ransomwhat.telemetry.ltd/groups"
+r = requests.get(url)
+groups_raw = r.json()
+groups ={}
+for group in groups_raw:
+    groups[group['name']] = None
+    for location in group['locations']:
+        if location['available']:
+            groups[group['name']] = location['fqdn']
+            break
+    
+
 url = "https://ransomwhat.telemetry.ltd/posts"
 r = requests.get(url)
 template = env.get_template("temp1.html")
 ransoms = r.json()
 ransoms.reverse()
+
+for ransom in ransoms:
+    try:
+        ransom['group_fqdn'] = groups[ransom['group_name']]
+    except KeyError:
+        ransom['group_fqdn'] = None
+
 with open('./index.html','w') as f:
             f.write(template.render(ransoms=ransoms,fecha=dt.now(tz=timezone.utc).strftime('%d-%b-%Y %H:%M %Z')))
